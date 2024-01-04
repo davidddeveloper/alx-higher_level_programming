94b9c4ad602dc2bd573b8a31867a4123ccb3a984
davidddeveloper <programmingdonebydavid@gmail.com>
2024-01-03 18:39:07 -0800

    3-print_alphabt.py

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+alphabet = "".join(
+        chr(char_code) for char_code in range(ord('a'), ord('z') + 1)
+        if chr(char_code) not in ('p', 'e')
+    )
+alphabet[4:5]
+print("{}".format(alphabet), end='')
