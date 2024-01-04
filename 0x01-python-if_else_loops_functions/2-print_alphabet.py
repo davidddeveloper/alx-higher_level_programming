8404508cd08a89be36aa460db659a5a53bf1106e
davidddeveloper <programmingdonebydavid@gmail.com>
2024-01-03 18:29:18 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    print(chr(char), end="")
+alphabet = "".join(chr(char_code) for char_code in range(ord('a'), ord('z') + 1))
+print("{}".format(alphabet), end='')
