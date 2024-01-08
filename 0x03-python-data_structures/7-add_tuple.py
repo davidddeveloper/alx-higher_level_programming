9e15f3ebe67282f9389ef125d611dadc1bcf8c07
davidddeveloper <programmingdonebydavid@gmail.com>
2024-01-08 04:06:01 -0800

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    length_tup_a = len(tuple_a)
+    length_tup_b = len(tuple_b)
+
+    if length_tup_a == 0 and length_tup_b == 0:
+        new_tuple = (0, 0)
+    if length_tup_a < 2 and length_tup_b < 2:
+        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+    if length_tup_a == 1:
+        new_tuple = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
+    if length_tup_b == 1:
+        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+    if length_tup_a == 0:
+        new_tuple = (0 + tuple_b[0], 0 + tuple_b[1])
+    if length_tup_b == 0:
+        new_tuple = (tuple_a[0] + 0, tuple_a[1] + 0)
+    if length_tup_a == 2 and length_tup_b == 2:
+        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+    return new_tuple
