8d1cc027ae0619e2df161734cda276f02b4e55d6
davidddeveloper <programmingdonebydavid@gmail.com>
2024-01-08 06:58:53 -0800

    10-check_cycle.c lists.h

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,8 +1,16 @@
 #include "lists.h"
 #include <stdio.h>
+
+/**
+ * check_cycle - checks if their is a repetitive sequence in a linked list
+ * @list: the linked list to check for repetitive sequence
+ * Return: 1 if there exist a repetitive sequence
+ * 0 if none exist
+ */
 int check_cycle(listint_t *list)
 {
 	listint_t *head;
+
 	head = list;
 	while (head != NULL)
 	{
