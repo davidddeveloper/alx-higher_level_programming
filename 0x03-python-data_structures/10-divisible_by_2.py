b03508dc26a78ac7fc5fa169915d18708b7a76f0
davidddeveloper <programmingdonebydavid@gmail.com>
2024-01-08 05:05:46 -0800

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = my_list.copy()
+    for element in new_list:
+        idx = my_list.index(element)
+        if element % 2 == 0:
+            new_list[idx] = True
+        else:
+            new_list[idx] =  False
+    return(new_list)
