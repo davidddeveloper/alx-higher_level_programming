4541977714f0e0e331f5fe9209272b8191f80c54
davidddeveloper <programmingdonebydavid@gmail.com>
2024-01-24 11:47:42 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+"""1-square.py
+
+This module define functionality for creating a square using oop
+
+It contains the following classes:
+    - Square: Represent the method for creating a square
+
+"""
+
+
 class Square:
+    """Represent the square
+
+    Args:
+        size: size of square
+
+    Attributes:
+        __size: size of square
+
+    """
+
     def __init__(self, size):
         self.__size = size
