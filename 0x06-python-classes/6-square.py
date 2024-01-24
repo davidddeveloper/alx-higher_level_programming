b7618cf4a04fdf34ca16dc74a001bd4378d43dfe
davidddeveloper <programmingdonebydavid@gmail.com>
2024-01-24 14:43:41 -0800

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -91,5 +91,6 @@ class Square:
         if self.__size == 0:
             print()
         for i in range(self.__size):
-            print(" " * self.__position[0], end="")
+            if getattr(self, self.__position, 0) <= 0:
+                print(" " * self.__position[0], end="")
             print("#" * self.__size)
