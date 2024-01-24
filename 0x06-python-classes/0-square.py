519058f7832caf85532e8c2b79a7c577e5fadbff
davidddeveloper <programmingdonebydavid@gmail.com>
2024-01-24 11:35:29 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""0-square.py
+
+This module provides functionality related to squares
+
+It contains the following classes:
+    - Square: Represents a square and provide methods for calculations.
+
+"""
+
+
 class Square:
     """Represents a square.
 
