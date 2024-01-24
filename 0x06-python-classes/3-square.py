b774b4cde41b0591b5b31053e2938102b8c7357f
davidddeveloper <programmingdonebydavid@gmail.com>
2024-01-24 12:26:49 -0800

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""3-square.py
+
+This module provide functionality related to squares
+
+classed define in module:
+    - Square: Represents a square and provides method for calculations
+
+"""
+
+
+class Square:
+    """Represents a square and provides method for calculations
+
+    Args:
+        - size: size of square
+
+    Attributes:
+        - __size: size of square
+
+    """
+
+    def __init__(self, size=0):
+        if size == f"{size}":
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
