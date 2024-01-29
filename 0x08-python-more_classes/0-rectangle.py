2d26cf3cb4d25d054182c03a774a537ca17854fc
davidddeveloper <programmingdonebydavid@gmail.com>
2024-01-29 07:32:13 -0800

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+"""0-rectangle.py
+    This module containes function classes related to Rectangle
+
+    classes define in it:
+        - Rectangle: provides attributes for creating a rectangle
+
+"""
+
+class Rectangle:
+    """provides attributes for creating a rectangle
+
+    """
+    pass
