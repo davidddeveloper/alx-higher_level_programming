d2f2262ef5156deccc46a812beb1657894d440d3
davidddeveloper <programmingdonebydavid@gmail.com>
2024-01-29 16:06:37 -0800

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -58,7 +58,7 @@ class Rectangle:
         if type(value) != int:
             raise TypeError("height must be an integer") from None
         if value < 0:
-            raise ValueError("width must be >= 0") from None
+            raise ValueError("height must be >= 0") from None
         self.__height = value
 
     def area(self):
