c9befe3926e78caa4c1d69c7f9e8ca311ec1f5ac
davidddeveloper <programmingdonebydavid@gmail.com>
2024-02-01 05:38:38 -0800

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -11,8 +11,12 @@ functions it contained:
 
 def say_my_name(first_name, last_name=""):
     if type(first_name) != str:
-        raise TypeError("first_name must be a string or last_name must be a string")
+        raise TypeError(
+                "first_name must be a string or last_name must be a string"
+            )
     if type(last_name) != str:
-        raise TypeError("first_name must be a string or last_name must be a string")
+        raise TypeError(
+                "first_name must be a string or last_name must be a string"
+            )
 
     print("My name is {} {}".format(first_name, last_name))
