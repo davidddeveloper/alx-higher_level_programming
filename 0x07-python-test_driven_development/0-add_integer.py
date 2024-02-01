83c11ea3806c56ffacca6b8480823b037fa5ecc5
davidddeveloper <programmingdonebydavid@gmail.com>
2024-02-01 02:27:22 -0800

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""0-add_integer.py
+    __test__ = {"external", "tests/0-add_integer.txt"}
+"""
+
+
+def add_integer(a, b=98):
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    if a == None:
+        raise TypeError("a must be an integer")
+    if type(b) != int:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
