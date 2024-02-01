29dfcbc5ce88c7b330dfee512fea7db32c02325a
davidddeveloper <programmingdonebydavid@gmail.com>
2024-02-01 06:23:47 -0800

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,6 +10,16 @@ function defined in square:
 
 
 def print_square(size):
+    """prints a square with the #
+    
+    Args:
+        - size: size of the square
+
+    Raises:
+        TypeError: size must be an integer
+        ValueError: size must be >= 0
+    """
+
     if type(size) != int:
         raise TypeError("size must be an integer")
     if size < 0:
