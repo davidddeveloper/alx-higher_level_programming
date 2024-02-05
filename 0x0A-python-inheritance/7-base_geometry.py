0b3692f66382d9cd956de9b9ddfadc26745bed2c
davidddeveloper <programmingdonebydavid@gmail.com>
2024-02-05 14:56:39 -0800

    created 7-base_gometry.py and a test for it in tests/7-base_geometry.txt

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""7-base_geometry.py
+
+This module contain classes and methods related to geometry
+
+classes it contain:
+    - BaseGeometry: defines a geometry
+"""
+
+
+class BaseGeometry:
+    """Represents a Geometry"""
+
+    def area(self):
+        """Calculates the area of a geometry"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Checks if value is an integer
+
+        Args:
+            - name: describe the content of the value
+            - value: an integer value
+
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less or equal to 0
+        """
+        if isinstance(value, int) is not True:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
