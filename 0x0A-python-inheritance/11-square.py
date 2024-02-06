ebe0b746bc228add756e43d7eaed45e4e963ec4f
davidddeveloper <programmingdonebydavid@gmail.com>
2024-02-05 16:33:04 -0800

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""10-square.py
+
+This module contains classes and methods related to square
+
+classes contained in it:
+    - Square: Represent a square
+
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square
+
+    Args:
+        - size: the size of the square
+
+    Attributes:
+        - __size: the size of the square
+
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: is size is less than or equal to zero
+
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Calculates the area of the square
+
+        Returns:
+            the area of the square -> int
+
+        """
+
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] {self.__width}/{self.__height}"
