9600529fea060828ff372b9ce74a700201d65438
davidddeveloper <programmingdonebydavid@gmail.com>
2024-02-06 15:03:24 -0800

    writes to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""1-write_file.py
+
+This module provides function for writing to a file
+
+"""
+
+
+def write_file(filename="", text=""):
+    """Reads a text file and prints to stdout
+
+    Args:
+        - filename: the name of the file
+        - text: the content to write
+
+    """
+
+    with open(filename, 'w', encoding='UTF-8') as f:
+        print(f.write(text))
