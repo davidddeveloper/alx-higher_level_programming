1c0dda3ad43035655a262f092257db6702c56f1d
davidddeveloper <programmingdonebydavid@gmail.com>
2024-02-05 16:35:51 -0800

    overwritten the __str__ in base class

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -41,4 +41,4 @@ class Square(Rectangle):
         return self.__size ** 2
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] {self.__width}/{self.__height}"
+        return f"[{self.__class__.__name__}] {self.__size}/{self.__size}"
