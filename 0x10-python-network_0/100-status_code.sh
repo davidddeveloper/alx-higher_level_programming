94341a46662e9c6852d49a9540df875e493be976
davidddeveloper <programmingdonebydavid@gmail.com>
2024-04-25 16:47:09 -0700

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
-# a Bash script that sends a request to a URL passed as an argument
-# and displays only the status code of the response.
+#!/bin/bash
+# a Bash script that sends a request to a URL passed as an argument and displays only the status code of the response.
 curl -I -s -o /dev/null -w "%{http_code}" $1
