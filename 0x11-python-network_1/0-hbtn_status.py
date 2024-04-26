1eb0e87adeb7b6213f916a7790b135cd40312346
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-04-26 21:31:26 +0000

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,11 @@
+"""
+a Python script that fetches
+'https://alx-intranet.hbtn.io/status'
 
+"""
+import urllib.request as re
+
+
+url = 'https://alx-intranet.hbtn.io/status'
+with re.urlopen(url) as response:
+  print(response.read())
