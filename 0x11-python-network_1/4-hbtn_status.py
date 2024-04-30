ebe8e2747609a9ba37cf8f380674cae4527796d0
davidddeveloper <programmingdonebydavid@gmail.com>
2024-04-30 06:03:09 -0700

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    a Python script that fetches
+    https://alx-intranet.hbtn.io/status
+"""
+import requests as re
+
+
+if __name__ == "__main__":
+    r = re.get('https://alx-intranet.hbtn.io/status')
+    body = r.text
+
+    print("    - type: {}".format(type(body)))
+    print("    - content: {}".format(body))
