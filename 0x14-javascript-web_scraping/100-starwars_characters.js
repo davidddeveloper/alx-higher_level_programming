f2aa78944fd183087f41ed27fef7b8818029624a
davidddeveloper <programmingdonebydavid@gmail.com>
2024-06-04 07:43:04 -0700

    a script that prints all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const movieId = process.argv.slice(2)[0];
+const request = require('request');
+
+if (movieId !== undefined) {
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+  request.get({ url, json: true }, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    }
+
+    body.characters.forEach(characterUrl => {
+      request.get({ url: characterUrl, json: true }, (error, response, people) => {
+        if (error) {
+          console.log(error);
+        }
+        console.log(people.name);
+      });
+    });
+  });
+}
