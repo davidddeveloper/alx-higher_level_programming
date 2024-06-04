3368a99328dcd2f391d881d6e69c2b86474ef55a
davidddeveloper <programmingdonebydavid@gmail.com>
2024-06-04 05:07:07 -0700

    # JavaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const argv = process.argv.slice(2);
+const filePath = argv[10];
+
+if (filePath !== undefined) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) { console.log(err); } else { console.log(data); }
+  });
+}
