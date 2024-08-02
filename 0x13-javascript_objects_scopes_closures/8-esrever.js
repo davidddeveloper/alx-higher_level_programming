be733c02bd702bd95a546d9346db443ac9028abd
davidddeveloper <programmingdonebydavid@gmail.com>
2024-08-02 09:00:06 -0700

    reverses a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 
 exports.esrever = function (list) {
   const newArr = [];
-  for (let i = list.length - 1; i >= 0; i--;) {
+  for (let i = list.length - 1; i >= 0; i--) {
     newArr.push(list[i]);
   }
   return newArr;
