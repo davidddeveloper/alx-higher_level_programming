b9d130b83c092841ba12562c5b6106bdb3731435
davidddeveloper <programmingdonebydavid@gmail.com>
2024-08-02 15:02:31 -0700

    fetch the first state using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" a script that list all states in db
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    username = args[0]
+    password = args[1]
+    db_name = args[2]
+
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                username,
+                password,
+                db_name
+                )
+            )
+    # create a session for querying
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # list all states
+    states = session.query(State).all()
+    state = states[0]
+    print('{}: {}'.format(state.id, state.name))
+    session.close()
