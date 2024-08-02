7208850ffc84807f33262ed0ef2f4fe44c95f05f
davidddeveloper <programmingdonebydavid@gmail.com>
2024-08-02 04:55:41 -0700

    fetch all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,17 +8,23 @@
 import MySQLdb
 import sys
 
-args = sys.argv[1:]
-username = args[0]
-password = args[1]
-db_name = args[2]
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    username = args[0]
+    password = args[1]
+    db_name = args[2]
 
-db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=db_name)
+    db = MySQLdb.connect(
+            host='localhost',
+            user=username,
+            passwd=password,
+            db=db_name
+        )
 
-cur = db.cursor()
+    cur = db.cursor()
 
-cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states")
 
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
