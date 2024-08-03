a5d3771371d82895b3e92bbb9293e6c4ff6c1f0c
davidddeveloper <programmingdonebydavid@gmail.com>
2024-08-02 19:51:55 -0700

    sorted dict

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,8 +5,8 @@ const dict = require('./101-data.js').dict;
 const newDict = {};
 for (const key of Object.keys(dict)) {
   if (newDict[dict[key]]) {
-    newDict[dict[key]].push(key);}
-  else {
+    newDict[dict[key]].push(key);
+  } else {
     newDict[dict[key]] = [key];
   }
 }
