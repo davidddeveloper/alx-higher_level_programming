7bb206abab7ffa85678660632aa167454aa4af8e
davidddeveloper <programmingdonebydavid@gmail.com>
2024-08-02 19:14:03 -0700

    using map in javascript

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-data = require('./100-data.js').list;
+const data = require('./100-data.js').list;
 console.log(data);
 
-new_data = data.map(function (val, idx) {
+let = data.map(function (val, idx) {
   return (val * idx);
 });
 console.log(new_data);
